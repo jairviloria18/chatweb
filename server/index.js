f08e1e02a5de9db57bc77b0e8f4a169813ac748c
jairviloria18 <jairviloria97@hotmail.com>
2024-11-20 20:20:33 -0500

    update url

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ const server = http.createServer(app);
 // Configurar Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "chatweb-hjjc2hcwx-jair-viloria-bertels-projects.vercel.app",
   },
 });
 
